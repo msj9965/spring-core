0e94cf6bfab6b90abaf5d521f37026692d1da376
moonseungju <msj9965@gmail.com>
2025-12-26 15:21:35 +0900

    test: singleton Configuration으로 보장되는지 확인

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,16 +14,26 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+    // 2번 호출되지만 같은 인스턴스가 공유됨
     @Bean
     public MemberService memberService(){
+
+        System.out.println("call AppConfig.memberService");
+
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
+
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService(){
+
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
