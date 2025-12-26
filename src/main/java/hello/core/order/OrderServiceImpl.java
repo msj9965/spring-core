db3dd7ecfdad2fe466f3c131c0f02b11f3c931e3
moonseungju <msj9965@gmail.com>
2025-12-26 16:54:55 +0900

    feat: 컴포넌트 스캔을 통한 빈 등록, 자동 의존관계 주입 해보기

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,10 +4,14 @@ import hello.core.discount.DiscountPolicy;
 
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository ;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
