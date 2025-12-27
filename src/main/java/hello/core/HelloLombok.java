b99ceb6deb4f6a7538334691097e97aefc2776c2
moonseungju <msj9965@gmail.com>
2025-12-27 20:39:10 +0900

    feat: Lombok 의존성 추가, 테스트

---
+++ src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("lombok");
+        helloLombok.setAge(20);
+        System.out.println("name = " + helloLombok.getName());
+        System.out.println("age = " + helloLombok.getAge());
+
+    }
+}
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -15,7 +15,6 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
